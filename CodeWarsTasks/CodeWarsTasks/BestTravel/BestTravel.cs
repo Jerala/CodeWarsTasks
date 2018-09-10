e4149278b6b0f1a6c7ac51cc62c199902c390993
Mikhail <jerala@mail.ru>
2018-09-10 23:02:42 +0300

    -

---
+++ CodeWarsTasks/CodeWarsTasks/BestTravel/BestTravel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodeWarsTasks.BestTravel
+{
+    class BestTravel
+    {
+        public static Int32 chooseBestSum(int t, int k, List<Int32> ls)
+        {
+            if (ls.Count < k || k == 0) return -1;
+            List<Int32> distances = getDists(k, ls);
+            distances.Sort();
+            if (distances.Contains(t)) return t;
+            for (int i = distances.Count - 1; i >= 0; i--)
+                if (distances[i] < t) return distances[i];
+            return -1;
+        }
+        public static List<Int32> getDists(int k, List<Int32> list)
+        {
+            List<Int32> dists = new List<Int32>();
+            if (k == 1)
+            {
+                foreach (int i in list)
+                    dists.Add(i);
+                return dists;
+            }
+            int prevSize = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                int currElem = list[i];
+                dists.AddRange(getDists(k - 1, list.GetRange(1 + i, list.Count)));
+                for (int j = prevSize; j < dists.Count; j++)
+                    dists[j] = dists[j] + currElem;
+                prevSize = dists.Count;
+            }
+            return dists;
+        }
+    }
+}
---
+++ CodeWarsTasks/CodeWarsTasks/CodeWarsTasksTests/BestTravelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeWarsTasks.CodeWarsTasksTests
+{
+    [TestFixture]
+    class BestTravelTest
+    {
+        [Test]
+        public void TestCase()
+        {
+
+        }
+    }
+}
